0d3edd1454934c525cb6128e3b001187da861929
hlnicks <hlnicks@icloud.com>
2022-09-20 22:10:20 -0400

    pseudocode schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { User, Book } = require("../models");
+
+const resolvers = {
+    // query
+
+    // mutation: save/remove books, add user, login/logout
+}
+
+// module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+const { gql } = require('apollo-server-express');
+
+// user, book, mutation
+const typeDefs = gql`
+
+`;
+
+// module.exports = typeDefs;
